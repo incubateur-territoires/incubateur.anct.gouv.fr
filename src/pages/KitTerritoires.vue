73862458bb8351a4cca5062dc311cbadee30d471
Jonathan Pichot <jonathan@pichot.us>
2020-10-01 12:27:35 +0200

    use airtable for contact form

--- src/pages/KitTerritoires.vue
+++ src/pages/KitTerritoires.vue
@@ -11,7 +11,12 @@
       <PageContent v-html="$page.pageContent.content" />
 
       <div class="buttons">
-        <a title="Télécharger le kit" class="matomo_download" href="/kitelus_def.pdf" target="_blank" id="downloadpdfbutton" onclick="_paq.push(['trackEvent', 'KitTerritoires', 'Download']);">Télécharger le kit</a>
+        <a class="button matomo_download" title="Télécharger le kit" href="/kitelus_def.pdf" target="_blank" id="downloadpdfbutton" onclick="_paq.push(['trackEvent', 'KitTerritoires', 'Download']);">Télécharger le kit</a>
+        <div class="button cursor-pointer" v-on:click="showContact = !showContact">Questions? Contactez-nous</div>
+      </div>
+
+      <div class="my-12 max-w-screen-md mx-auto" v-if="showContact">
+        <iframe class="airtable-embed airtable-dynamic-height" src="https://airtable.com/embed/shr1fmbfwYDtsKX7l?backgroundColor=orange" frameborder="0" onmousewheel="" width="100%" height="533" style="background: transparent; border: 1px solid #ccc;"></iframe>
       </div>
     </div>
   </Layout>
@@ -32,7 +37,7 @@ query page {
   .buttons {
     @apply mt-12 font-semibold;
 
-    a:first-child {
+    .button:first-child {
       @apply mb-4;
 
       @screen sm {
@@ -44,7 +49,7 @@ query page {
       @apply flex justify-center mb-6;
     }
 
-    a {
+    .button {
       @apply block text-center;
 
       @apply py-2 px-4 bg-yellow rounded-lg text-navy border-4 border-yellow text-lg transition ease-linear duration-100;
@@ -66,9 +71,19 @@ export default {
   metaInfo: {
     title: "Kit territoires",
   },
+  data() {
+    return {
+      showContact: false
+    }
+  },
   components: {
     PageTitle,
     PageContent,
+  },
+  mounted() {
+    let recaptchaScript = document.createElement('script')
+    recaptchaScript.setAttribute('src', 'https://static.airtable.com/js/embed/embed_snippet_v1.js')
+    document.head.appendChild(recaptchaScript)
   },
 };
 </script>
