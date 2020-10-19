8b18187d145402f5210e0363e1c0460c9b489e25
Jonathan Pichot <jonathan@pichot.us>
2020-10-19 11:28:31 +0200

    generate actualite

---
+++ src/templates/Actualite.vue
@@ -0,0 +1,20 @@
+<template>
+  <Layout>
+    <div class="actualite">
+      <PageTitle>
+        {{$page.actualite.title}}
+      </PageTitle>
+
+      <PageContent v-html="$page.actualite.content" />
+    </div>
+  </Layout>
+</template>
+
+<page-query>
+query ($id: ID!) {
+  actualite(id: $id) {
+    title
+    content
+  }
+}
+</page-query>
