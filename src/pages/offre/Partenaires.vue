a3b409bc865b9240e7352abf3be95de3419afb82
Jonathan Pichot <jonathan@pichot.us>
2020-10-30 18:40:21 +0100

    fix

--- src/pages/offre/Partenaires.vue
+++ src/pages/offre/Partenaires.vue
@@ -32,12 +32,12 @@ export default {
       meta: [
         // Twitter
         {key:'twitter:title', property: 'twitter:title', content: `Offres aux acteurs publics et partenaires de collectivités`},
-        {key:'twitter:description', property: 'twitter:description', content: `${this.$page.job.equipe} recrute : ${this.$page.job.role}`},
+        {key:'twitter:description', property: 'twitter:description', content: `Accompagnement de projets sur mesure`},
         {key:'twitter:image', property: 'twitter:image', content: `https://incubateur.anct.gouv.fr/og-image-offre-partenaires.png`},
         // OpenGraph
         {key:'og:title', property: 'og:title', content: `Offres aux acteurs publics et partenaires de collectivités`},
         {key:'og:site_name', property: 'og:site_name', content: `Incubateur des Territoires`},
-        {key:'og:description', property: 'og:description', content: `Les collectivités peuvent à tout moment solliciter l'Incubateur des Territoires`},
+        {key:'og:description', property: 'og:description', content: `Accompagnement de projets sur mesure`},
         {key:'og:image', property: 'og:image', content: `https://incubateur.anct.gouv.fr/og-image-offre-partenaires.png`},
       ]
     }
