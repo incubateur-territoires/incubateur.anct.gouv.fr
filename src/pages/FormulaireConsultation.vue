3698f8bc8bc8035c840178a9bc0e13436ae50e2e
Jonathan Pichot <jonathan@pichot.us>
2021-04-08 15:02:18 +0200

    airtable dynamic height

--- src/pages/FormulaireConsultation.vue
+++ src/pages/FormulaireConsultation.vue
@@ -5,7 +5,7 @@
     </div>
 
     <div class="my-12 max-w-screen-md mx-auto">
-      <iframe class="airtable-embed" src="https://airtable.com/embed/shrfUfZTzwyllSjgg?backgroundColor=pink" frameborder="0" onmousewheel="" width="100%" height="533" style="background: transparent; border: 1px solid #ccc;"></iframe>
+      <iframe class="airtable-embed airtable-dynamic-height" src="https://airtable.com/embed/shrfUfZTzwyllSjgg?backgroundColor=pink" frameborder="0" onmousewheel="" width="100%" height="533" style="background: transparent; border: 1px solid #ccc;"></iframe>
     </div>
   </Layout>
 </template>
