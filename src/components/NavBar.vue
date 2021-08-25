f84a165f0adc3e7ab31fc2dcd9255c82a3356f45
Vincent Agnano <vinyll@protonmail.com>
2021-08-25 11:45:01 +0200

    fixed menu icon color

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -180,7 +180,7 @@
                 </g-link>
                 <g-link to="https://forum.incubateur.anct.gouv.fr" class="-m-3 p-3 flex items-start space-x-4 rounded-lg hover:bg-gray-200 transition ease-in-out duration-150">
                   <!-- Heroicon name: chat-alt-2 -->
-                  <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <svg class="flex-shrink-0 h-6 w-6 text-blue" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z" />
                   </svg>
                   <div class="space-y-1">
