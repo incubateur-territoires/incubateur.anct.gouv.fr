fd7c953bdb977efcb2f7b665d6a9d41fff7c9b54
Vincent Agnano <vinyll@protonmail.com>
2021-10-11 15:41:30 +0200

    nav link to forum with 'new' tag

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -21,7 +21,6 @@
         <svg class="text-gray-500 h-5 w-5 group-hover:text-gray-600 group-focus:text-gray-600 transition ease-in-out duration-150" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
           <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
         </svg>
-        <Tag class="md:absolute md:-top-5 md:-right-5">Nouveau</Tag>
       </button>
 
       <transition
@@ -121,8 +120,13 @@
       </transition>
     </div>
 
-       <a href to="https://forum.incubateur.anct.gouv.fr"class=""><button class="button space-x-2"><span>Forum</span></button></a>
-        </nav>
+    <a href="https://forum.incubateur.anct.gouv.fr" class="relative">
+      <button class="button space-x-2">
+        <span>Forum</span>
+        <Tag class="md:absolute md:-top-6 md:-right-8">Nouveau</Tag>
+      </button>
+    </a>
+  </nav>
 </template>
 
 <static-query>
