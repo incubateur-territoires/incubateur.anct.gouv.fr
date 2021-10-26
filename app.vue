94b25588b44eb54b0195852788d1ea4b326336c9
Vincent Agnano <vinyll@protonmail.com>
2021-10-26 07:02:16 +0200

    added footer and callout in the main section

--- app.vue
+++ app.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
     <Header />
+    <main class="fr-container fr-py-6w">
+      <div class="fr-grid-row">
+        <div class="fr-col-12">
+          <CallOut />
+        </div>
+      </div>
+    </main>
+    <Footer />
   </div>
 </template>
