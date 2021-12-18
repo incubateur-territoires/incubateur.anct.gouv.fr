967eb90909895966d15514a091c6184c48f2a7f2
Vincent Agnano <vinyll@protonmail.com>
2021-12-18 20:11:28 +0100

    actually filter services by theme

--- pages/index.vue
+++ pages/index.vue
@@ -9,26 +9,20 @@
           <div class="fr-grid-row fr-grid-row--gutters fr-py-6w">
             <div class="fr-col">
               <h4>Type de collectivité</h4>
-              <Checkbox @change="filter('types', 'region', $event)" label="Région" />
-              <Checkbox @change="filter('types', 'departement', $event)" label="Département" />
-              <Checkbox @change="filter('types', 'epci', $event)" label="EPCI" />
-              <Checkbox @change="filter('types', 'commune', $event)" label="Commune" />
+              <Checkbox v-for="decoupages_administratifs in facets_groups.decoupages_administratifs" @change="filter('decoupages_administratifs', decoupages_administratifs.id, $event)" :label="decoupages_administratifs.nom" />
             </div>
 
             <div class="fr-col">
-              <h4>Taille de collectivité</h4>
-              <Checkbox @change="filter('populations', '3500', $event)" label="< 3500 habitants" />
-              <Checkbox @change="filter('populations', '10000', $event)" label="< 10.000 habitants" />
-              <Checkbox @change="filter('populations', '100000', $event)" label="< 100.000 habitants" />
+              <h4>Thématique</h4>
+              <Checkbox v-for="thematique in facets_groups.thematiques" @change="filter('thematiques', thematique.id, $event)" :label="thematique.thematique" />
             </div>
           </div>
-
           <h3>{{ services.length }} services sont susceptibles de vous intéresser</h3>
           <div class="fr-grid-row fr-grid-row--gutters">
             <div class="fr-col-lg-3 fr-col-md-4 fr-col-sm-12" v-for="service in services">
               <Card
                 :title="service.nom"
-                :description="service.type"
+                :description="service.description"
                 image="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.WXUbigVrd4W8p067r5e6ggAAAA%26pid%3DApi&f=1"
               />
             </div>
@@ -42,16 +36,23 @@
 
 <script>
   export default {
+    created() {
+      this.$store.dispatch('services/init')
+    },
 
     computed: {
       services() {
         return this.$store.state.services.list
-      }
+      },
+      facets_groups() {
+        return this.$store.state.services.facets_groups
+      },
     },
 
     methods: {
       filter(name, value, active) {
-        this.$store.commit('services/filter', name, value, active)
+        this.$store.dispatch('services/filter', { name, value, active })
+        //this.$store.dispatch('services/fetch')
       }
     }
   }
--- store/services.js
+++ store/services.js
@@ -1,19 +1,91 @@
 export const state = () => ({
   list: [],
-  filters: {
-    types: [],
-    populations: [],
+  facets_groups: {
+    thematiques: [],
+    decoupages_administratifs: [],
+  },
+  facets: {
+    thematiques: [],
+    decoupages_administratifs: [],
   },
 })
 
+export const actions = {
+  async init({ state, commit }) {
+    const response = await this.$http.$post('https://directus.incubateur.anct.gouv.fr/graphql?access_token=confidant-ample-slapping-vitamins-freewill-unlivable', {
+        query: `
+          query {
+            catalogue {
+              nom
+              description
+              thematique {
+                id
+                thematique
+              }
+            }
+            decoupage_administratif {
+              id
+              nom
+            }
+            thematiques {
+              id
+              thematique
+            }
+          }`
+        }
+      )
+      commit('init', {
+        services: response.data.catalogue,
+        decoupages_administratifs: response.data.decoupage_administratif,
+        thematiques: response.data.thematiques
+      })
+  },
+
+  async fetch({ state, commit }) {
+    const filters = []
+    if(state.facets.thematiques.length) {
+      filters.push(`thematique: { id: { _in: ${JSON.stringify(state.facets.thematiques.map(v => Number(v)))}}}`)
+    }
+
+    const response = await this.$http.$post('https://directus.incubateur.anct.gouv.fr/graphql?access_token=confidant-ample-slapping-vitamins-freewill-unlivable', {
+      query: `
+        query {
+          catalogue(filter: { ${filters.join(', ')} }) {
+            nom
+            description
+            thematique {
+              id
+              thematique
+            }
+          }
+        }`
+      }
+    )
+    commit('update', response.data.catalogue || [])
+
+  },
+
+  async filter({ state, commit, dispatch }, facet) {
+    commit('addFilter', facet)
+    await dispatch('fetch')
+    commit('update', state.list)
+  },
+
+}
+
 export const mutations = {
-  init(state, services) {
+  init(state, facets_groups) {
+    state.list = facets_groups.services
+    state.facets_groups = facets_groups
+  },
+
+  update(state, services) {
     state.list = services
   },
 
-  filter(state, name, value, active) {
+  addFilter(state, { name, value, active }) {
     active
-      ? state.filters[name].push(value)
-      : state.filters[name] = state.filters[name].filter(k => k !== value)
+      ? state.facets[name].push(value)
+      : state.facets[name] = state.facets[name].filter(k => k !== value)
   },
 }
