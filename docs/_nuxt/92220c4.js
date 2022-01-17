beec6e8edf3546a6ee7e65193db94f972fccc4be
Vincent Agnano <vinyll@protonmail.com>
2022-01-17 11:20:36 +0100

    add docs for deployment

---
+++ docs/_nuxt/92220c4.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{321:function(e,t,n){"use strict";n.r(t);var c=n(34),component=Object(c.a)({},(function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)}),[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",[n("h1",[e._v("Les actualités")])])}],!1,null,null,null);t.default=component.exports}}]);
---
+++ docs/_nuxt/static/1642414765/actualites/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/actualites", {data:[{}],fetch:{},mutations:[]});
