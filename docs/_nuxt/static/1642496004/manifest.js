176a9c7adffba168b95437bcfc7820fc13f5bef2
Vincent Agnano <vinyll@protonmail.com>
2022-01-19 12:43:49 +0100

    deploy website

--- docs/_nuxt/static/1642496004/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Factualites","\u002F","\u002Fcatalogue","\u002Frecrutement","\u002Fservices"]})
---
+++ docs/_nuxt/static/1642592588/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Frecrutement","\u002Factualites","\u002Fservices","\u002Fcatalogue"]})
